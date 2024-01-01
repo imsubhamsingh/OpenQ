42278da454cad7954dc988c2b4053d5d30eba1cf
Subham Singh <geekysubham@gmail.com>
2024-01-02 04:53:46 +0530

    refactor producer and consumer into classes and fix typos

--- openq/openq.py
+++ openq/openq.py
@@ -11,7 +11,15 @@ class Message:
         self.received_at = None
 
 
-class VisiblityTimeOutExpired(Exception):
+class VisibilityTimeOutExpired(Exception):
+    """Raised when the visibility timeout for a message has expired."""
+
+    pass
+
+
+class MessageNotFoundError(Exception):
+    """Raised when the message ID is not found."""
+
     pass
 
 
@@ -20,7 +28,7 @@ class OpenQ:
     A Simple Python in-memory message Queue Service
     """
 
-    def __init__(self, name, visibility_timeout=30, dlq=None):
+    def __init__(self, name, visibility_timeout=300, dlq=None):
         self.name = name
         self.messages = deque()
         self.visibility_timeout = visibility_timeout
@@ -59,13 +67,13 @@ class OpenQ:
                 for message in self.messages:
                     if message.id == message_id:
                         if self._visibility_timeout_expired(message):
-                            raise VisiblityTimeOutExpired(
+                            raise VisibilityTimeOutExpired(
                                 "Visibility timeout expired; message requeued."
                             )
                         self.messages.remove(message)  # Remove message from queue
                         print(f"Acknowledged Task ID: {message.id}")
                         return
-        raise VisiblityTimeOutExpired("Message ID not found or already acknowledged.")
+        raise VisibilityTimeOutExpired("Message ID not found or already acknowledged.")
 
     def _requeue(self, message):
         """
@@ -90,35 +98,65 @@ class OpenQ:
         return now >= expiry_time
 
 
-def producer(queue, num_tasks):
+class Producer(threading.Thread):
     """
-    Producing messages to queue
+    The Producer class is a subclass of Thread that simulates the production
+    of tasks. It puts task messages onto a provided queue and prints out a log
+    message for each task produced with its corresponding queue name and task ID.
+    Attributes:
+        queue (Queue): an instance of a queue class which has an enqueue method
+                    to add message to the queue.
+        num_tasks (int): the number of tasks to produce and put in the queue.
     """
-    for i in range(num_tasks):
-        message_body = f"Task {i}"
-        message_id = queue.enqueue(message_body)
-        print(f"Produced to Queue: {queue.name} Task ID: {message_id}")
-        time.sleep(1)
 
+    def __init__(self, queue, num_tasks):
+        super().__init__()
+        self.queue = queue
+        self.num_tasks = num_tasks
 
-def consumer(queue):
-    """
-    Consumes message and listen to queue
-    """
-    print("### CONSUMER ###")
-    while True:
-        message = queue.dequeue()
-        if message:
-            print(f"Consumed Task ID: {message.id}, Body: {message.body}")
-            try:
-                # Simulate processing time
-                time.sleep(2)
-                queue.delete(message.id)
-            except VisiblityTimeOutExpired as e:
-                print(str(e))
-        else:
-            print(f"{queue.name} is empty, waiting for tasks...")
+    def run(self):
+        """
+        Producing messages to queue
+        """
+        for i in range(self.num_tasks):
+            message_body = f"Task {i}"
+            message_id = self.queue.enqueue(message_body)
+            print(f"📩 Produced to Queue: {self.queue.name} Task ID: {message_id}")
             time.sleep(1)
+
+
+class Consumer(threading.Thread):
+    """
+    The Consumer class is a subclass of Thread that simulates the consumption
+    of tasks. It retrieves task messages from the provided queue, processes them,
+    and then deletes them from the queue.
+
+    Attributes:
+        queue (Queue): an instance of a queue class which has dequeue and delete
+                    methods to remove and process messages from the queue.
+    """
+
+    def __init__(self, queue):
+        super().__init__()
+        self.queue = queue
+
+    def run(self):
+        """
+        Consumes message and listens to queue
+        """
+        while True:
+            message = self.queue.dequeue()
+            if message:
+                print(f"📨 Consumed Task ID: {message.id}, Body: {message.body}")
+                try:
+                    # Simulate processing time
+                    time.sleep(2)
+                    self.queue.delete(message.id)
+                except VisibilityTimeOutExpired as e:
+                    print(str(e))
+            else:
+                print(f"😴 Queue {self.queue.name} is empty, waiting for tasks...")
+                time.sleep(1)
 
 
 ## Queue Creation Junction ##
@@ -131,11 +169,15 @@ main_queue = OpenQ(name="main-queue", visibility_timeout=10, dlq=dlq)
 num_tasks = 5
 
 # Using threading to simulate concurrent producing and consuming processes
-producer_thread = threading.Thread(target=producer, args=(main_queue, num_tasks))
-consumer_thread = threading.Thread(target=consumer, args=(main_queue,))
+producer_thread = Producer(main_queue, num_tasks)
+consumer_thread = Consumer(main_queue)
 
 producer_thread.start()
 consumer_thread.start()
 
+
+# The join() method is a synchronization mechanism that ensures that the main program waits
+# for the threads to complete before moving on. Calling join() on producer_thread causes the
+# main thread of execution to block until producer_thread finishes its task.
 producer_thread.join()
 consumer_thread.join()
